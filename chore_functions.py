91ae35baa7953ab2b5ba5e6fa362552597404411
unknown <doug.rokakis@gmail.com>
2023-03-14 16:19:58 -0400

    updates to chore function gui

--- chore_functions.py
+++ chore_functions.py
@@ -1,12 +1,12 @@
-FILEPATH = 'todos.txt'
+FILEPATH = 'chores.txt'
 
-def get_todos(filepath=FILEPATH):
+def get_chores(filepath=FILEPATH):
     """ reads selected file and returns the list of to-do items. THIS IS AN EXAMPLE OF A DOC STRING."""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath=FILEPATH):
+def write_chores(todos_arg, filepath=FILEPATH):
     """ Writes to end of filepath what is written in todos_arg """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -1,12 +1,25 @@
-from functions import get_todos, write_todos
+import chore_functions
 import PySimpleGUI as sg
 
 
 
 label = sg.Text("Add something to your chore list")
-input_box = sg.InputText(tooltip="Enter chore")
+input_box = sg.InputText(tooltip="Enter chore", key="chore")
 add_button = sg.Button("Add")
 
-window = sg.Window('Chore App', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('Chore App', 
+                layout=[[label], [input_box, add_button]], font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            chores = chore_functions.get_chores()
+            new_chore = values['chore'] + '\n'
+            chores.append(new_chore)
+            chore_functions.write_chores(chores)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
